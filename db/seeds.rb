f05d8f232d30cb56e3e0e9c22e0ce75712124e8a
hanbyeol kwon <janviertrois@naver.com>
2023-04-07 13:15:28 -0400

    Adjust landing page row margin right and col7 padding right

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,9 @@
 puts 'Creating user account...'
   user = User.new(
-    email: "oliviatangredixo@gmail.com",
-    password: "123456",
-    last_name: "Tangredi",
-    first_name: "Olivia"
-  )
-  user.save!
-  user = User.new(
-    email: "dreijanuar@gmail.com",
+    email: "me_me_me_me@naver.com",
     password: "123456",
     last_name: "Kwon",
-    first_name: "Bie"
-  )
-  user.save!
-  user = User.new(
-    email: "anthonyborremans@hotmail.com",
-    password: "123456",
-    last_name: "Borremans",
-    first_name: "Anthony"
+    first_name: "Hanbyeol"
   )
   user.save!
 puts 'users accounts create'
